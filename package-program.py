e33fc3d93b9299bb474a658721d2cbff82678474
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-10-31 00:49:51 +0000

    made good changes

--- package-program.py
+++ package-program.py
@@ -12,22 +12,22 @@ import math
 def main():
 
     # input
-    package = int(input("Enter weight of package (kg): "))
+    package_weight = int(input("Enter weight of package (kg): "))
     package_length = int(input("Enter package length (cm): "))
     package_width = int(input("Enter package width (cm): "))
     package_height = int(input("Enter package height (cm): "))
 
     # process and output
-    if (package > 27) or (package < 27):
+    if (package_weight > 27) or (package_weight < 27):
         print(
             "\n{0:,}kg is an approved weight, the company accepts package.".format(
-                package
+                package_weight
             )
         )
     else:
         print(
             "\n{0:,}kg is unapproved weight, the company rejects package.".format(
-                package
+                package_weight
             )
         )
     volume = package_length * package_width * package_height
@@ -37,7 +37,13 @@ def main():
         )
     else:
         print(
-            "\n{0:,} cubic cm is unapproved, the company rejects package".format(volume)
+            "\n{0:,} cubic cm is unapproved, the company rejects package.".format(
+                volume
+            )
+        )
+    if package_weight == 27 or volume > 10000:
+        print(
+            "\nSorry the company cannot accepted the package because its equal to 27kg or larger than 10,000 cubic cm."
         )
 
     print("\n\nDone.")
